803f9375a10190b8dbfe980ad6bcfef5adf70c6c
Tom ROBERT <tomrobert.etudiant@gmail.com>
2022-10-11 15:22:48 +0200

    initialisation du serveur

---
+++ server.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let app =express();
+let port=3000;
+
+app.listen(port,()=>{
+    console.log('le serveur est en route');
+    console.log('Serveur listening at http://localhost:${port}');
+})
+
+app.get('/',(req,res,next)=>{
+    res.send('Bonjour !');
+})
