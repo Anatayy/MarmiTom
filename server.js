6a827336d78eb4e5fdc8b2e78b58b6c7763821fd
Tom ROBERT <tomrobert.etudiant@gmail.com>
2022-10-18 14:36:05 +0200

    Ajout des recettes 7 et 8

--- server.js
+++ server.js
@@ -55,6 +55,20 @@ let recette3 = {
 }
 
 
+let recette7 = {
+    nomRecette : "Poisson au curry express" ,
+    photo:"images/recettes/recette7.jpeg",
+    categorie : "poisson",
+    ingredients : [["Curry",0.5,"Cuillère à soupe"],["gousse d'ail",0.5],["huile",0.75,"cuillère à soupe"],["filet de colin",1],["lait de coco",5,"cl"]],
+    preparation :"ÉTAPE 1 <br/> Dans une sauteuse ou une poêle, faire chauffer à feu doux l'huile, l'ail pilé et les épices pendant 2 min. <br/> ÉTAPE 2 <br/> Déposer les filets de poisson congelés. Les retourner après 4 min, et verser le lait de coco. <br/>ÉTAPE 3 <br/>Remuer la sauce. <br/>ÉTAPE 4 <br/>C'est prêt !"
+}
 
+let recette8 = {
+    nomRecette : "Poisson au lait de coco" ,
+    photo:"images/recettes/recette8.jpg",
+    categorie : "poisson",
+    ingredients : [["tomate",1],["oignon",0.25],["oignon",0.25],["curcuma (facultatif)"],["fillet de poisson blanc",1],["boite de lait de coco",0.25]],
+    preparation :"ÉTAPE 1 <br/> Faire revenir les oignons coupés fin dans une grande poêle antiadhésive. <br/> ÉTAPE 2 <br/> Ajouter les filets de poisson roulés sur eux-mêmes, les faire revenir doucement 5 à 6 minutes. Saler et poivrer. <br/>ÉTAPE 3 <br/>Ajouter les tomates coupées en petits dés. Cuire à feu doux 5 minutes. <br/>ÉTAPE 4 <br/>Ajouter le lait de coco et le curcuma (facultatif). Laisser mijoter doucement 10 à 15 minutes sans remuer."
+}
 
 
