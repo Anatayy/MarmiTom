1b6f6e316d708763912a53c4fe5b17b378211f99
Raphaelle Artaud <raphaelle.artaud@univ-rouen.fr>
2022-10-18 14:06:27 +0200

    résolution d'erreur perso

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let port=3000;
 
 app.use(express.static(__dirname+'/www'));
 app.use('/js', express.static( __dirname + '/node_modules/bootstrap/dist/js') ) ;
+app.use('/js', express.static( __dirname + '/node_modules/jquery/dist') ) ;
 app.use('/css', express.static( __dirname + '/node_modules/bootstrap/dist/css') ) ;
 
 app.listen(port,()=>{
@@ -11,14 +12,30 @@ app.listen(port,()=>{
     console.log('Serveur listening at http://localhost:${port}');
 })
 
+/*
+app.get('/',(req,res,next)=>{
+    res.send('Bonjour totos !');
+})
+*/
 
+/*
+app.get('/',(req,res,next)=>{
+    res.sendFile('www/index.html');
+});
 
+*/
 
 app.set('view engine','ejs');
 app.use('/views',express.static(__dirname + '/views'));
 
+
+//passage d'un objet dans la vue de la page 2
+
+let myObject = {
+    nom : "monobjet" ,
+    valeur : 10
+}
     
-// LIEN DES PAGES
 
 app.get('/',(req,res,next)=>{
     res.render('index.ejs');
@@ -31,11 +48,3 @@ app.get('/page2',(req,res,next)=>{
     res.render('page3.ejs',{monobjet:myObject});
 })
 
-
-
-//RECETTES
-
-let rec1 = {
-    nomRecette : "monobjet" ,
-    ingrédients : ["ingredient","ingredient"]
-}
